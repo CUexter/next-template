46d8dedcb04009c2865ed1e29767b24e1587e11e
Longinus Erste <2554539-hk724@users.noreply.gitlab.com>
2023-01-27 18:42:27 +0800

    feat!: back to stable nextjs
    BREAKING_CHANGE: app directory is removed

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import Page from "./page";
+import Index from "@/pages/index";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
   it("renders a heading", () => {
-    render(<Page />);
+    render(<Index />);
 
     const heading = screen.getByRole("heading", {
       name: /testing/i,
